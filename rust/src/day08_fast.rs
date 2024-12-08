d599041c95110eb7ee0159eb348cac2226517f05
teevik <teemu.vikoren@gmail.com>
2024-12-08 16:45:27 +0100

    vek

--- rust/src/day08_fast.rs
+++ rust/src/day08_fast.rs
@@ -1,13 +1,3 @@
-#![feature(slice_as_chunks)]
-#![feature(portable_simd)]
-
-use arrayvec::ArrayVec;
-use itertools::Itertools;
-use rustc_hash::{FxHashMap, FxHashSet};
-use std::{
-    mem::{MaybeUninit, transmute},
-    simd::{cmp::SimdPartialEq, u8x64},
-};
 use vek::{Aabr, Vec2};
 
 const WIDTH: usize = 50;
@@ -18,207 +8,117 @@ const BOUNDS: Aabr<i32> = Aabr {
     max: Vec2::new(WIDTH as i32 - 1, HEIGHT as i32 - 1),
 };
 
-pub fn part1(input: &str) -> usize {
+pub fn part1(input: &str) -> u32 {
     let input = input.as_bytes();
 
-    // let mut antennas = FxHashMap::<u8, FxHashSet<Vec2<i32>>>::default();
+    let mut antinodes: [u64; HEIGHT] = const { [0; HEIGHT] };
 
-    // let lines = unsafe { input.as_chunks_unchecked::<WIDTH_WITH_NEWLINE>() };
+    for character in (b'A'..=b'Z').chain(b'a'..=b'z').chain(b'0'..=b'9') {
+        let mut memchr = memchr::Memchr::new(character, input);
 
-    const POG: usize = (b'z' - b'0') as usize + 1;
-    // let antennas = const { [ArrayVec::new_const(); POG] };
+        let a = match memchr.next() {
+            Some(a) => a,
+            None => continue,
+        };
 
-    let mut antennas: [ArrayVec<Vec2<i32>, 10>; POG] = {
-        let mut array: [MaybeUninit<ArrayVec<Vec2<i32>, 10>>; POG] =
-            [const { MaybeUninit::uninit() }; POG];
+        let b = unsafe { memchr.next().unwrap_unchecked() };
+        let c = unsafe { memchr.next().unwrap_unchecked() };
+        let d = unsafe { memchr.next().unwrap_unchecked() };
 
-        let mut i: usize = 0;
-        while i < POG {
-            // <-- Note 1
-            array[i] = MaybeUninit::new(ArrayVec::new_const());
-            i += 1;
-        }
-        // SAFETY: We initialised each `MaybeUninit` in the loop so we can `assume_init`
-        unsafe { transmute(array) } // <-- Note 2
-    };
+        let indices = [a, b, c, d];
+        let [a_pos, b_pos, c_pos, d_pos] = indices.map(|index| {
+            let x = index % WIDTH_WITH_NEWLINE;
+            let y = index / WIDTH_WITH_NEWLINE;
 
-    let mut antinodes = [false; WIDTH_WITH_NEWLINE * HEIGHT];
-    let antinode_index =
-        |pos: Vec2<i32>| -> usize { pos.y as usize * WIDTH_WITH_NEWLINE + pos.x as usize };
+            Vec2::new(x as i32, y as i32)
+        });
 
-    let mut sum = 0;
+        let combinations = [
+            (a_pos, b_pos),
+            (a_pos, c_pos),
+            (a_pos, d_pos),
+            (b_pos, c_pos),
+            (b_pos, d_pos),
+            (c_pos, d_pos),
+        ];
 
-    let chunks = unsafe { input.as_chunks_unchecked::<WIDTH_WITH_NEWLINE>() };
-    // find any chars that are not '.'
-    let mask = u8x64::splat(b'.');
-    let zeros = u8x64::splat(b'0');
+        for (a, b) in combinations {
+            let delta = b - a;
 
-    for (y, chunk) in chunks.into_iter().enumerate() {
-        let chunk = unsafe { chunk.get_unchecked(..WIDTH) };
-        let chunk = u8x64::load_or(chunk, mask);
+            let left_antinode = b + delta;
+            let right_antinode = a - delta;
 
-        let correct_ones = chunk.simd_ne(mask);
-
-        let pog = chunk - zeros;
-
-        dbg!(pog);
-    }
-
-    for (i, &character) in input.into_iter().enumerate() {
-        if character != b'\n' && character != b'.' {
-            let antenna_index = character - b'0';
-            // dbg!(pos, character as char);
-
-            let antennas = &mut antennas[antenna_index as usize];
-
-            let x = i % WIDTH_WITH_NEWLINE;
-            let y = i / WIDTH_WITH_NEWLINE;
-            let pos = Vec2::new(x as i32, y as i32);
-
-            for &antenna in antennas.iter() {
-                let delta = pos - antenna;
-
-                let left_antinode = pos + delta;
-                let right_antinode = antenna - delta;
-
-                if BOUNDS.contains_point(left_antinode) {
-                    let left_antinode_index = antinode_index(left_antinode);
-                    let left_antinode_slot =
-                        unsafe { antinodes.get_unchecked_mut(left_antinode_index) };
-
-                    if *left_antinode_slot == false {
-                        *left_antinode_slot = true;
-                        sum += 1;
-                    }
-                }
-
-                if BOUNDS.contains_point(right_antinode) {
-                    let right_antinode_index = antinode_index(right_antinode);
-                    let right_antinode_slot =
-                        unsafe { antinodes.get_unchecked_mut(right_antinode_index) };
-
-                    if *right_antinode_slot == false {
-                        *right_antinode_slot = true;
-                        sum += 1;
-                    }
-                }
+            if BOUNDS.contains_point(left_antinode) {
+                let spot = unsafe { antinodes.get_unchecked_mut(left_antinode.y as usize) };
+                *spot |= 1 << left_antinode.x;
             }
 
-            unsafe { antennas.push_unchecked(pos) };
-
-            //         let delta = b - a;
-
-            //         let left_antinode = b + delta;
-            //         let right_antinode = a - delta;
-
-            //         if BOUNDS.contains_point(left_antinode) {
-            //             antinodes.insert(left_antinode.y as usize * WIDTH + left_antinode.x as usize);
-            //         }
-
-            //         if BOUNDS.contains_point(right_antinode) {
-            //             antinodes.insert(right_antinode.y as usize * WIDTH + right_antinode.x as usize);
-            //         }
-
-            // dbg!(character as char);
-        }
-    }
-
-    // for (y, line) in lines.into_iter().enumerate() {
-    //     let line = unsafe { line.get_unchecked(..WIDTH) };
-
-    //     for (x, &character) in line.iter().enumerate() {
-    //         if character != b'.' {
-    //             // antennas
-    //             //     .entry(character)
-    //             //     .or_default()
-    //             //     .insert(Vec2::new(x as i32, y as i32));
-    //         }
-    //     }
-    // }
-
-    // // let mut antinodes = FxHashSet::default();
-    // let mut antinodes = BitSet::with_capacity(WIDTH * HEIGHT);
-
-    // for antennas in antennas.values() {
-    //     let combinations = antennas.iter().tuple_combinations::<(_, _)>();
-
-    //     for (a, b) in combinations {
-    //         let delta = b - a;
-
-    //         let left_antinode = b + delta;
-    //         let right_antinode = a - delta;
-
-    //         if BOUNDS.contains_point(left_antinode) {
-    //             antinodes.insert(left_antinode.y as usize * WIDTH + left_antinode.x as usize);
-    //         }
-
-    //         if BOUNDS.contains_point(right_antinode) {
-    //             antinodes.insert(right_antinode.y as usize * WIDTH + right_antinode.x as usize);
-    //         }
-    //     }
-    // }
-
-    // antinodes.len()
-    sum
-}
-
-fn find_antennas(input: &str) -> FxHashMap<char, FxHashSet<Vec2<i32>>> {
-    let mut antennas = FxHashMap::<char, FxHashSet<Vec2<i32>>>::default();
-
-    for (y, line) in input.lines().enumerate() {
-        for (x, character) in line.chars().enumerate() {
-            if character != '.' {
-                antennas
-                    .entry(character)
-                    .or_default()
-                    .insert(Vec2::new(x as i32, y as i32));
+            if BOUNDS.contains_point(right_antinode) {
+                let spot = unsafe { antinodes.get_unchecked_mut(right_antinode.y as usize) };
+                *spot |= 1 << right_antinode.x;
             }
         }
     }
 
-    antennas
+    antinodes.into_iter().map(|x| x.count_ones()).sum()
 }
 
-fn find_bounds(input: &str) -> Aabr<i32> {
-    let width = input.lines().next().unwrap().len() as i32;
-    let height = input.lines().count() as i32;
+pub fn part2(input: &str) -> u32 {
+    let input = input.as_bytes();
 
-    let bounds = Aabr {
-        min: Vec2::zero(),
-        max: Vec2::new(width - 1, height - 1),
-    };
+    let mut antinodes: [u64; HEIGHT] = const { [0; HEIGHT] };
 
-    bounds
-}
+    for character in (b'A'..=b'Z').chain(b'a'..=b'z').chain(b'0'..=b'9') {
+        let mut memchr = memchr::Memchr::new(character, input);
 
-pub fn part2(input: &str) -> usize {
-    let antennas = find_antennas(input);
-    let bounds = find_bounds(input);
+        let a = match memchr.next() {
+            Some(a) => a,
+            None => continue,
+        };
 
-    let mut antinodes = FxHashSet::default();
+        let b = unsafe { memchr.next().unwrap_unchecked() };
+        let c = unsafe { memchr.next().unwrap_unchecked() };
+        let d = unsafe { memchr.next().unwrap_unchecked() };
 
-    for antennas in antennas.values() {
-        let combinations = antennas.iter().tuple_combinations::<(_, _)>();
+        let indices = [a, b, c, d];
+        let [a_pos, b_pos, c_pos, d_pos] = indices.map(|index| {
+            let x = index % WIDTH_WITH_NEWLINE;
+            let y = index / WIDTH_WITH_NEWLINE;
 
-        for (&a, &b) in combinations {
+            Vec2::new(x as i32, y as i32)
+        });
+
+        let combinations = [
+            (a_pos, b_pos),
+            (a_pos, c_pos),
+            (a_pos, d_pos),
+            (b_pos, c_pos),
+            (b_pos, d_pos),
+            (c_pos, d_pos),
+        ];
+
+        for (a, b) in combinations {
             let delta = b - a;
 
             let mut a = a;
             let mut b = b;
 
-            while bounds.contains_point(a) {
-                antinodes.insert(a);
+            while BOUNDS.contains_point(a) {
+                let spot = unsafe { antinodes.get_unchecked_mut(a.y as usize) };
+                *spot |= 1 << a.x;
+
                 a -= delta;
             }
 
-            while bounds.contains_point(b) {
-                antinodes.insert(b);
+            while BOUNDS.contains_point(b) {
+                let spot = unsafe { antinodes.get_unchecked_mut(b.y as usize) };
+                *spot |= 1 << b.x;
                 b += delta;
             }
         }
     }
 
-    antinodes.len()
+    antinodes.into_iter().map(|x| x.count_ones()).sum()
 }
 
 #[cfg(test)]
