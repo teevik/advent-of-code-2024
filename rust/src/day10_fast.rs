2b15b53c33169459a35dbc835c1394302f5cd014
teevik <teemu.vikoren@gmail.com>
2024-12-10 13:23:07 +0100

    day10_fast

--- rust/src/day10_fast.rs
+++ rust/src/day10_fast.rs
@@ -1,67 +1,14 @@
-use memchr::{Memchr, arch::x86_64};
+use memchr::Memchr;
 
 const SIZE: usize = 58;
 const WIDTH_WITH_NEWLINE: usize = SIZE + 1;
 
 /// Recursively search trails
-fn search_trail(grid: &[u8], start_position: usize, start_cell: u8, ends: &mut [u64; SIZE]) {
-    const NEIGHBORS: [isize; 4] = [
-        -1,
-        1,
-        -(WIDTH_WITH_NEWLINE as isize),
-        WIDTH_WITH_NEWLINE as isize,
-    ];
-
-    for neighbor in NEIGHBORS {
-        let target_position = (start_position as isize + neighbor) as usize;
-
-        if target_position < const { WIDTH_WITH_NEWLINE * SIZE } {
-            let target_cell = *unsafe { grid.get_unchecked(target_position) };
-
-            if target_cell == start_cell + 1 {
-                if target_cell == b'9' {
-                    // let n = unsafe { ends.get_unchecked_mut(target_position) };
-                    // *n += 1;
-                    let y = target_position / WIDTH_WITH_NEWLINE;
-                    let x = target_position % WIDTH_WITH_NEWLINE;
-
-                    let row = unsafe { ends.get_unchecked_mut(y) };
-                    *row |= 1 << x;
-                } else {
-                    search_trail(grid, target_position, target_cell, ends);
-                }
-            }
-        }
-    }
-}
-
-pub fn part1(input: &str) -> u32 {
-    let input = input.as_bytes();
-    let search = Memchr::new(b'0', input);
-
-    let mut sum = 0;
-
-    let mut trail_ends = [0u64; SIZE];
-    for start in search {
-        let cell = *unsafe { input.get_unchecked(start) };
-
-        search_trail(input, start, cell, &mut trail_ends);
-
-        for trail_end in &mut trail_ends {
-            sum += trail_end.count_ones();
-            *trail_end = 0;
-        }
-    }
-
-    sum
-}
-
-/// Recursively search trails
-fn search_trail_2(
+fn search_trail(
     grid: &[u8],
     start_position: usize,
     start_cell: u8,
-    ends: &mut [u8; SIZE * WIDTH_WITH_NEWLINE],
+    found_trail_end: &mut impl FnMut(usize),
 ) {
     const NEIGHBORS: [isize; 4] = [
         -1,
@@ -78,14 +25,40 @@ fn search_trail_2(
 
             if target_cell == start_cell + 1 {
                 if target_cell == b'9' {
-                    let n = unsafe { ends.get_unchecked_mut(target_position) };
-                    *n += 1;
+                    found_trail_end(target_position);
                 } else {
-                    search_trail_2(grid, target_position, target_cell, ends);
+                    search_trail(grid, target_position, target_cell, found_trail_end);
                 }
             }
         }
     }
+}
+
+pub fn part1(input: &str) -> u32 {
+    let input = input.as_bytes();
+    let search = Memchr::new(b'0', input);
+
+    let mut sum = 0;
+
+    let mut trail_ends = [0u64; SIZE];
+    for start in search {
+        let cell = *unsafe { input.get_unchecked(start) };
+
+        search_trail(input, start, cell, &mut |target_position| {
+            let y = target_position / WIDTH_WITH_NEWLINE;
+            let x = target_position % WIDTH_WITH_NEWLINE;
+
+            let row = unsafe { trail_ends.get_unchecked_mut(y) };
+            *row |= 1 << x;
+        });
+
+        for trail_end in &mut trail_ends {
+            sum += trail_end.count_ones();
+            *trail_end = 0;
+        }
+    }
+
+    sum
 }
 
 pub fn part2(input: &str) -> u32 {
@@ -98,7 +71,10 @@ pub fn part2(input: &str) -> u32 {
     for start in search {
         let cell = *unsafe { input.get_unchecked(start) };
 
-        search_trail_2(input, start, cell, &mut trail_ends);
+        search_trail(input, start, cell, &mut |target_position| {
+            let n = unsafe { trail_ends.get_unchecked_mut(target_position) };
+            *n += 1;
+        });
 
         for trail_end in &mut trail_ends {
             sum += *trail_end as u32;
@@ -125,3 +101,38 @@ mod tests {
         assert_eq!(part2(INPUT), 1764);
     }
 }
+
+// fn search_trail_iter(
+//     grid: &[u8],
+//     start_position: usize,
+//     start_cell: u8,
+//     found_trail_end: &mut impl FnMut(usize),
+// ) {
+//     const NEIGHBORS: [isize; 4] = [
+//         -1,
+//         1,
+//         -(WIDTH_WITH_NEWLINE as isize),
+//         WIDTH_WITH_NEWLINE as isize,
+//     ];
+
+//     let mut queue = ArrayVec::<(usize, u8), 20>::new();
+//     queue.push((start_position, start_cell));
+
+//     while let Some((start_position, start_cell)) = queue.pop() {
+//         for neighbor in NEIGHBORS {
+//             let target_position = (start_position as isize + neighbor) as usize;
+
+//             if target_position < const { WIDTH_WITH_NEWLINE * SIZE } {
+//                 let target_cell = *unsafe { grid.get_unchecked(target_position) };
+
+//                 if target_cell == start_cell + 1 {
+//                     if target_cell == b'9' {
+//                         found_trail_end(target_position);
+//                     } else {
+//                         queue.push((target_position, target_cell));
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
